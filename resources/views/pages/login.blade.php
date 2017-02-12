8f65112102458463f9492f70e9b6b75d6990c7f1
Anton Smolko <asmolko@yandex.ru>
2017-02-12 21:47:33 +0300

    Правка шаблона login и registered

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,40 +1,41 @@
 @extends('pages.main')
 
 @push('styles')
-<link rel="stylesheet" type="text/css" href="../css/login.css">
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
 @endpush
-
-@section('header')
-@endsection
 
 @section('content')
     <div class="auth">
-        <h2>{{ $title }}</h2>
-        <form method="post">
-            {{ csrf_field() }}
-            <div class="auth-container">
-                <span class="auth--tag">Логин</span>
-                <input class="pages-iput" type="text" name="login" value="{{ old('login') }}">
-                @if ($errors->has('login'))
-                    <div class="danger"><span>{{ $errors->first('login') }}</span></div>
-                @endif
+        <div class="wrapper">
+            <div class="auth--item">
+                <h2>{{ $title }}</h2>
+                <form method="post">
+                    {{ csrf_field() }}
+                    <div class="auth--row">
+                        <span class="auth--tag">Логин</span>
+                        <input class="input auth-input" type="text" name="login" value="{{ old('login') }}">
+                        @if ($errors->has('login'))
+                            <div class="danger"><span>{{ $errors->first('login') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Пароль</span>
+                        <input class="input auth-input" type="password" name="password">
+                        @if ($errors->has('password'))
+                            <div class="danger"><span>{{ $errors->first('password') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Запомнить меня</span>
+                        <input type="checkbox" class="auth-input" name="remember">
+                    </div>
+                    <input class="button" type="submit" value="Войти">
+                    <a class="button" href="/">Отменить</a>
+                    @if(session('authError'))
+                        <div class="auth_danger">{{ session('authError') }}</div>
+                    @endif
+                </form>
             </div>
-            <div class="auth-container">
-                <span class="auth--tag">Пароль</span>
-                <input class="pages-iput" type="password" name="password">
-                @if ($errors->has('password'))
-                    <div class="danger"><span>{{ $errors->first('password') }}</span></div>
-                @endif
-            </div>
-            <div class="auth-container">
-                <span class="auth--tag">Запомнить меня</span>
-                <input type="checkbox" name="remember">
-            </div>
-            <input class="button" type="submit" value="Войти">
-            <a class="button" href="/">Отменить</a>
-            @if(session('authError'))
-                <div class="auth_danger">{{ session('authError') }}</div>
-            @endif
-        </form>
+        </div>
     </div>
 @endsection
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,54 +1,56 @@
 @extends('pages.main')
 
 @push('styles')
-<link rel="stylesheet" type="text/css" href="../css/login.css">
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
 @endpush
-
-@section('header')
-@endsection
 
 @section('content')
     <div class="auth">
-        <h2>{{ $title }}</h2>
-        <form method="post">
-            {{ csrf_field() }}
-            <div class="auth-container">
-                <span class="auth--tag">Имя</span>
-                <input class="pages-iput" type="text" name="name" value="{{ old('name') }}">
-                @if ($errors->has('name'))
-                    <div class="danger"><span>{{ $errors->first('name') }}</span></div>
-                @endif
+        <div class="wrapper">
+            <div class="auth--item">
+                <h2>{{ $title }}</h2>
+                <form method="post">
+                    {{ csrf_field() }}
+                    <div class="auth--row">
+                        <span class="auth--tag">Имя</span>
+                        <input class="input auth--input" type="text" name="name" value="{{ old('name') }}">
+                        @if ($errors->has('name'))
+                            <div class="danger"><span>{{ $errors->first('name') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Логин</span>
+                        <input class="input auth--input" type="text" name="login" value="{{ old('login') }}">
+                        @if ($errors->has('login'))
+                            <div class="danger"><span>{{ $errors->first('login') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Пароль</span>
+                        <input class="input auth--input" type="password" name="password">
+                        @if ($errors->has('password'))
+                            <div class="danger"><span>{{ $errors->first('password') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Повтор пароля</span>
+                        <input class="input auth--input" type="password" name="password2">
+                        @if ($errors->has('password2'))
+                            <div class="danger"><span>{{ $errors->first('password2') }}</span></div>
+                        @endif
+                    </div>
+                    <div class="auth--row">
+                        <span class="auth--tag">Email</span>
+                        <input class="input auth--input" type="text" name="email" value="{{ old('email') }}">
+                        @if ($errors->has('email'))
+                            <div class="danger"><span>{{ $errors->first('email') }}</span></div>
+                        @endif
+                    </div>
+                    <input class="button" type="submit" value="Зарегистрироваться">
+                    <a class="button" href="/">Отменить</a>
+                </form>
             </div>
-            <div class="auth-container">
-                <span class="auth--tag">Логин</span>
-                <input class="pages-iput" type="text" name="login" value="{{ old('login') }}">
-                @if ($errors->has('login'))
-                    <div class="danger"><span>{{ $errors->first('login') }}</span></div>
-                @endif
-            </div>
-            <div class="auth-container">
-                <span class="auth--tag">Пароль</span>
-                <input class="pages-iput" type="password" name="password">
-                @if ($errors->has('password'))
-                    <div class="danger"><span>{{ $errors->first('password') }}</span></div>
-                @endif
-            </div>
-            <div class="auth-container">
-                <span class="auth--tag">Пароль</span>
-                <input class="pages-iput" type="password" name="password2">
-                @if ($errors->has('password2'))
-                    <div class="danger"><span>{{ $errors->first('password2') }}</span></div>
-                @endif
-            </div>
-            <div class="auth-container">
-                <span class="auth--tag">Email</span>
-                <input class="pages-iput" type="text" name="email" value="{{ old('email') }}">
-                @if ($errors->has('email'))
-                    <div class="danger"><span>{{ $errors->first('email') }}</span></div>
-                @endif
-            </div>
-            <input class="button" type="submit" value="Зарегистрироваться">
-            <a class="button" href="/">Отменить</a>
-        </form>
+        </div>
+
     </div>
 @endsection
