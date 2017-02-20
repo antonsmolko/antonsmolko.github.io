b587d82bfc125ec96e3b67131bf208d7ba70c072
Anton Smolko <asmolko@yandex.ru>
2017-02-20 14:27:55 +0300

    ФВВыаыва

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -20,12 +20,17 @@ class ArticleRepository
 
     public function getAllButLast()
     {
-        return Cache::remember('articlesButLast', env('CACHE_TIME', 0), function () {
-            return Article::where('id', '<>', $this->getLast()->id)
-                ->published()
-                ->latest()
-                ->paginate(config('blog.itemsPerPage'));
-        });
+        if ($this->getLast()) {
+            return Cache::remember('articlesButLast', env('CACHE_TIME', 0), function () {
+                return Article::where('id', '<>', $this->getLast()->id)
+                    ->published()
+                    ->latest()
+                    ->paginate(config('blog.itemsPerPage'));
+            });
+        }
+
+        return false;
+
     }
 
     public function getOne($id)
